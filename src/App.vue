bed815fc53146fa45f3d9dfeccf8287d14314dc6
liaoyz999999 <46314483+liaoyz999999@users.noreply.github.com>
2021-06-30 17:42:27 +0800

    uploader first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <global-uploader></global-uploader>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import globalUploader from './components/globalUploader'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    globalUploader
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import uploader from 'vue-simple-uploader'
 
+const app = createApp(App)
+app.use(uploader)
+ 
 createApp(App).mount('#app')
